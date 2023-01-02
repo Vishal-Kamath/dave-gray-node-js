eb393635b6558a2be1567db59521786499cc3510
Vishal-Kamath <vishalkamath853@gmail.com>
2023-01-02 23:02:29 +0530

    video 2: read, write file using async await

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 // video 2
-const fs = require('fs');
+const fsPromises = require('fs').promises;
 const path = require('path');
 
-fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log(data);
-})
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8');
+    console.log(data);
+    await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt')); // deletes the file
 
-console.log('Hello....') // To show the async behavoiur of node
+    await fsPromises.writeFile(path.join(__dirname, 'files', 'promiseWrite.txt'), data);
+    await fsPromises.appendFile(path.join(__dirname, 'files', 'promiseWrite.txt'), '\n\nNice to meet you');
+    await fsPromises.rename(path.join(__dirname, 'files', 'promiseWrite.txt'), path.join(__dirname, 'files', 'promiseComplete.txt'));
+    const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'promiseComplete.txt'), 'utf8');
+    console.log(newData);
+  } catch(err) {
+    console.log(err);
+  }
+}
 
-// exit on uncaught error 
-process.on('uncaughtException', err => {
-  console.log(`There was an uncaught Error: ${err}`);
-  process.exit(1);
-})
-
-fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'Nice to meet you' , (err) => {
-  if (err) throw err;
-  console.log('Write complete');
-
-  fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), '\n\nYes it is' , (err) => {
-    if (err) throw err;
-    console.log('Append complete');
-
-    fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt') , (err) => {
-      if (err) throw err;
-      console.log('Rename complete');
-    })
-  })
-})
+fileOps();
