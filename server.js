0dad630fb796333d4e78127979455b6f7a2ce57f
Vishal-Kamath <vishalkamath853@gmail.com>
2023-01-04 11:32:34 +0530

    express video 2: middlewares

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const logEvents = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
 
-// Route handlers 
+// Custom Middleware logger
+app.use((req, res, next) => {
+  logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+  console.log(`${req.method} ${req.path}`);
+  next();
+})
+
+// Middleware
+// built in middleware to handle urlencoded data
+// in other words, form data;
+// 'content-type': 'application/x-www-form-urlencoded4
+app.use(express.urlencoded({extended: false}));
+
+// built in middleware for json
+app.use(express.json());
+
+// server static files
+app.use(express.static(path.join(__dirname, '/public')));
+// makes static files available to public
+// search localhost:3500/css/style.css
+
 app.get('^/$|/index(.html)?' , (req, res) => { 
-  // ^/ must begin with a slash
-  // /$ must end with a slash
-  //  |/index.html or it must be /index.html
-  // (.html)? makes .html optional
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
@@ -17,12 +34,9 @@ app.get('/new-page(.html)?' , (req, res) => {
 });
 
 app.get('/old-page(.html)?' , (req, res) => {
-  res.redirect(301, '/new-page.html');  // 302 by default
-                                        // 301 mean it is perminently redirected to new page 
+  res.redirect(301, '/new-page.html');
 });
 
-// chaining route handler
-// method 1
 app.get('/hello(.html)?', (req, res, next) => {
   console.log('attempted to call hello.html');
   next();
@@ -30,7 +44,7 @@ app.get('/hello(.html)?', (req, res, next) => {
   res.send('Hello World');
 });
 
-// method 2
+
 const one = (req, res, next) => {
   console.log('one');
   next();
@@ -47,13 +61,10 @@ const three = (req, res, next) => {
 app.get('/chain(.html)?' , [one, two, three]);
 
 
-// ERROR
 app.get('/*', (req, res) => {
   res.status(404)
   res.sendFile(path.join(__dirname, 'views', '404.html'));
-  // chianing the status code as express doesnt know its a 404 as it could find the 404.html page
 });
-
 
 
 app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
