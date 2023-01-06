823970a5bf87139f581ec5b8f6d8aeecb1c4867f
Vishal-Kamath <vishalkamath853@gmail.com>
2023-01-06 20:33:36 +0530

    express video 7: roles

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,51 +1,67 @@
 const usersDB = {
-  users: require('../model/users.json'),
-  setUsers: function (data) {this.users = data}
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
 };
 
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
-const fsPromises = require('fs').promises;
-const path = require('path');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const fsPromises = require("fs").promises;
+const path = require("path");
 
 const handleLogin = async (req, res) => {
-  const {user, pwd} = req.body;
-  if(!user || !pwd) return res.status(400).json({"message": "user and password are required"});
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res.status(400).json({ message: "user and password are required" });
 
-  const foundUser = usersDB.users.find(person => person.username === user);
+  const foundUser = usersDB.users.find((person) => person.username === user);
   if (!foundUser) return res.sendStatus(401);
 
   // evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
+    const roles = Object.values(foundUser.roles);
     // create JWTs
     const accessToken = jwt.sign(
-      {"username": foundUser.username},   // Payload
-      process.env.ACCESS_TOKEN_SECRET,    // secret key
-      {expiresIn: '30s'}                     // expire
+      {
+        "UserInfo": {
+          "username": foundUser.username,
+          "roles": roles,
+        },
+      }, // Payload
+      process.env.ACCESS_TOKEN_SECRET, // secret key
+      { expiresIn: "30s" } // expire
     );
     const refreshToken = jwt.sign(
-      {"username": foundUser.username},
+      { "username": foundUser.username },
       process.env.REFRESH_TOKEN_SECRET,
-      {expiresIn: '1d'}                   
+      { expiresIn: "1d" }
     );
 
     // saving refresh token with user
-    const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
-    const currentUser = {...foundUser, refreshToken};
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
     usersDB.setUsers([...otherUsers, currentUser]);
     await fsPromises.writeFile(
-      path.join(__dirname, '..', 'model', 'users.json'),
+      path.join(__dirname, "..", "model", "users.json"),
       JSON.stringify(usersDB.users)
     );
 
-    res.cookie('jwt', refreshToken, {httpOnly: true, sameSite: 'None', secrure: true ,maxAge: 24 * 60 * 60 * 1000});
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      sameSite: "None",
+      secrure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
     // jwt name of cookie / key
     // refreshTOken / value
     // set parameters such as
     // httpOnly: true - cannot be accessed by javascript
-    // sameSite: 'None' - by default the cookie will be stored in the same site domain but very often the 
+    // sameSite: 'None' - by default the cookie will be stored in the same site domain but very often the
     //                    the frontend and backend will have different site domains so the it will by default try to set the
     //                    on the backend domain but but none it can be set on other sites
     // secure: true - https only / will tranfer only on secure
@@ -54,6 +70,6 @@ const handleLogin = async (req, res) => {
   } else {
     res.sendStatus(401);
   }
-}
+};
 
-module.exports = {handleLogin};
+module.exports = { handleLogin };
--- controllers/registeredController.js
+++ controllers/registeredController.js
@@ -1,41 +1,44 @@
 const usersDB = {
-  users: require('../model/users.json'),
-  setUsers: function (data) {this.users = data}
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
 };
 
-const fsPromises = require('fs').promises;
-const path = require('path');
-const bcrypt = require('bcrypt');
+const fsPromises = require("fs").promises;
+const path = require("path");
+const bcrypt = require("bcrypt");
 
 const handleNewUser = async (req, res) => {
-  const {user, pwd} = req.body;
-  if(!user || !pwd) return res.status(400).json({"message": "user and password are required"});
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res.status(400).json({ message: "user and password are required" });
 
   // check for duplicate usernames
-  const duplicate = usersDB.users.find(person => person.username === user);
+  const duplicate = usersDB.users.find((person) => person.username === user);
   if (duplicate) return res.sendStatus(409); // Conflict
-  
+
   try {
     // encrypt the password
     const hashedPassword = await bcrypt.hash(pwd, 10);
 
     // store the new user
     const newUser = {
-      "username": user,
-      "password": hashedPassword
+      username: user,
+      roles: { User: 2001 },
+      password: hashedPassword,
     };
     usersDB.setUsers([...usersDB.users, newUser]);
 
     await fsPromises.writeFile(
-      path.join(__dirname, '..', 'model', 'users.json'),
+      path.join(__dirname, "..", "model", "users.json"),
       JSON.stringify(usersDB.users)
     );
     console.log(usersDB.users);
-    res.status(201).json({'success': `New user ${user} created`});
-
-  } catch(err) {
-    res.status(500).json({"message": err.message});
+    res.status(201).json({ success: `New user ${user} created` });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
-}
+};
 
-module.exports = {handleNewUser};
+module.exports = { handleNewUser };
